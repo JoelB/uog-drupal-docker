313d7958401a2e2a1e4794649bf9a5f0ab69408d
Joel Best <jbest@uoguelph.ca>
2018-03-11 15:22:02 -0400

    Rename script

--- scripts/set-drupal-admin-password.sh
+++ scripts/set-drupal-admin-password.sh
@@ -5,5 +5,5 @@ if [[ $# != 1 ]] ; then
 fi
 
 export PWHASH=`docker exec -it uog-drupal-web scripts/password-hash.sh $1|grep 'hash: '|sed -e 's/.*hash: //' -e 's/\s.*//'`
-docker exec -i uog-drupal-mysql mysql -udrupal -p`cat ../env/mysql.override.env|grep MYSQL_PASSWORD|sed -e 's/.*=//'` `cat ../env/mysql.override.env|grep MYSQL_DATABASE|sed -e 's/.*=//'` -e "update users set pass = '${PWHASH}' where uid='1';"
+docker exec -i uog-drupal-mysql mysql -udrupal -p`cat ./env/mysql.override.env|grep MYSQL_PASSWORD|sed -e 's/.*=//'` `cat ./env/mysql.override.env|grep MYSQL_DATABASE|sed -e 's/.*=//'` -e "update users set pass = '${PWHASH}' where uid='1';"
 
